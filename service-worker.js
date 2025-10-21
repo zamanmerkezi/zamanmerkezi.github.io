285ab11827352bb6d4a751bda3ed7e09c1991cce
zamanmerkezi <zamanmerkezi@gmail.com>
2025-10-21 18:47:49 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,23 @@
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
+  console.log("Service Worker: Yüklendi");
   event.waitUntil(
-    caches.open("zaman-merkezi-v1").then((cache) => {
+    caches.open("zaman-merkezi-v1").then(cache => {
       return cache.addAll([
-        "index.html",
-        "manifest.json",
-        "script.js",
-        "style.css",
-        "icons/icon-192.png",
-        "icons/icon-512.png",
-        "hakkinda.html",
-        "gizlilik.html",
-        "iletisim.html"
+        "/",
+        "/index.html",
+        "/manifest.json",
+        "/style.css",
+        "/script.js",
+        "/icons/icon-192.png",
+        "/icons/icon-512.png"
       ]);
     })
   );
-  console.log("✅ Service Worker yüklendi.");
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
+    caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
   );
