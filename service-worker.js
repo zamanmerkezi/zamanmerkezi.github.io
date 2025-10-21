18b753436be215bc0084c1e0813f63f5081bff26
zamanmerkezi <zamanmerkezi@gmail.com>
2025-10-21 18:35:50 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,26 @@
-const CACHE_NAME = "zaman-merkezi-v1";
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
-];
-
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open("zaman-merkezi-v1").then((cache) => {
+      return cache.addAll([
+        "index.html",
+        "manifest.json",
+        "script.js",
+        "style.css",
+        "icons/icon-192.png",
+        "icons/icon-512.png",
+        "hakkinda.html",
+        "gizlilik.html",
+        "iletisim.html"
+      ]);
+    })
   );
-  self.skipWaiting();
+  console.log("✅ Service Worker yüklendi.");
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
